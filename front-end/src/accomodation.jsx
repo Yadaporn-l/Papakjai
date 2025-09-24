11c269cad814e97bf502d9212f1cb87bb5f82a51
Yadaporn.l <yadaporn.l@kkumail.com>
2025-09-25 05:17:58 +0700

    test

---
+++ front-end/src/accomodation.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavHome from './components/navhome'
+import Footer from './components/footer'
+export default function Accomodation() {
+  return (
+    <>
+      <NavHome/>
+        A
+      <Footer/>
+    </>
+    
+   
+  )
+}
