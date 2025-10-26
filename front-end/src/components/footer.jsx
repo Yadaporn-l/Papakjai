2c946855a924426ee0c5da0996b53bd2aad92fbf
Yadaporn.l <yadaporn.l@kkumail.com>
2025-10-26 11:34:35 +0700

    renew

--- front-end/src/components/footer.jsx
+++ front-end/src/components/footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import logo from '/pubilc/pa_pak_jai-removebg.png'
+
 export default function Footer() {
   return (
     <>
@@ -8,7 +8,7 @@ export default function Footer() {
             <div className="container f-top">
                 <div className="brandline">
                     <div className="logo">
-                        <img src={logo} alt="logo"></img>
+                        <img src="./img/pa_pak_jai-removebg.png" alt="logo" />
                         <span>PaPak<span className="accent">Jai</span></span>
                     </div>
                     <p>All-in-one travel picks, visa info, and lists for quick trips.</p>
